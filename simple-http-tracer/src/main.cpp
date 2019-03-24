489d03b6b78e76e72598e559fa708fb18aacaa0a
Mohammad Malihi Aqdam <mohammad.malihi@gmail.com>
2019-03-25 00:03:43 +0430

    Parse command line arguments.

--- simple-http-tracer/src/main.cpp
+++ simple-http-tracer/src/main.cpp
@@ -1,5 +1,52 @@
+#include <stdint.h>
+#include <iostream>
+
 #include <pcap.h>
 
-int main(int main, char **argv) {
+std::string pcapFileName;
+std::string captureDirectory;
+std::string ipToFilter;
+
+void ParseCommandLineArguments(int argc, char **argv) {
+
+	for (int i{}; i < argc; i++) {
+
+		if (strcmp(argv[i], "-f") == 0) {
+			if (i + 1 < argc) {
+				pcapFileName = argv[i + 1];
+			}
+			else {
+				std::cout << "pcap file name required" << std::endl;
+				std::cout << "usage [-s ip] [-o output directory] [-f pcap file name]\n" << std::endl;
+				exit(-1);
+			}
+		}
+		else if (strcmp(argv[i], "-o") == 0) {
+			if (i + 1 < argc) {
+				captureDirectory = argv[i + 1];
+			}
+			else {
+				std::cout << "output directory name required" << std::endl;
+				std::cout << "usage [-s ip] [-o output directory] [-f pcap file name]\n" << std::endl;
+				exit(-2);
+			}
+		}
+		else if (strcmp(argv[i], "-s") == 0) {
+			if (i + 1 < argc) {
+				ipToFilter = argv[i + 1];
+			}
+			else {
+				std::cout << "ip address required" << std::endl;
+				std::cout << "usage [-s ip] [-o output directory] [-f pcap file name]\n" << std::endl;
+				exit(-2);
+			}
+		}
+	}
+}
+
+int main(int argc, char **argv) {
+
+	ParseCommandLineArguments(argc, argv);
+
 	return 0;
 }
