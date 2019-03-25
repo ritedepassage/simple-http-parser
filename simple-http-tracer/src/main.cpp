dfd2ee4d7c5eb6a8f592dff2c4160d8affd4c0fe
Mohammad Malihi Aqdam <mohammad.malihi@gmail.com>
2019-03-25 16:13:10 +0430

    Obtain pcap handle as online considering available network interfaces.

--- simple-http-tracer/src/main.cpp
+++ simple-http-tracer/src/main.cpp
@@ -54,6 +54,54 @@ pcap_t *ObtainOfflinePcapHandle() {
 	return pcapFileHandle;
 }
 
+pcap_t *ObtainOnlinePcapHandle() {
+
+	pcap_if_t *networkInterfaces;
+	pcap_if_t *selectedInterface;
+	pcap_t *interfaceHandle = NULL;
+
+	if (pcap_findalldevs(&networkInterfaces, errorBuffer) == -1) {
+
+		std::cout << "failed to retreive all interfaces" << std::endl;
+		return NULL;
+	}
+
+	int interfacesCount{ 0 };
+
+	for (selectedInterface = networkInterfaces; selectedInterface != NULL; selectedInterface = selectedInterface->next) {
+
+		std::cout << std::endl;
+		std::cout << ++interfacesCount << "." << selectedInterface->name;
+
+		if (strlen(selectedInterface->description))
+			std::cout << "-- " << selectedInterface->description << std::endl;
+	}
+
+	int interfaceNumber{ -1 };
+
+	std::cout << "Enter desired interface number between 1 and " << interfacesCount << ":";
+	std::cin >> interfaceNumber;
+
+	if (interfaceNumber < 1 || interfaceNumber > interfacesCount)
+	{
+		std::cout << std::endl;
+		std::cout << "Interface number out of range." << std::endl;
+
+		pcap_freealldevs(networkInterfaces);
+
+		return NULL;
+	}
+
+	selectedInterface = networkInterfaces;
+
+	for (interfacesCount = 0; interfacesCount < interfaceNumber - 1; interfacesCount++)
+		selectedInterface = selectedInterface->next;
+
+	interfaceHandle = pcap_open(selectedInterface->name, 65536, PCAP_OPENFLAG_PROMISCUOUS, 1000, NULL, errorBuffer);
+
+	return interfaceHandle;
+}
+
 int main(int argc, char **argv) {
 
 	ParseCommandLineArguments(argc, argv);
@@ -71,7 +119,7 @@ int main(int argc, char **argv) {
 		}
 	}
 	else {
-
+		pcapHandle = ObtainOnlinePcapHandle();
 	}
 
 	return 0;
